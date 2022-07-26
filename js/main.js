57ea1f5b0ebaffe7ff56d5d8bf2f08c990e8d10c
Baz Murphy <bazmurphy@gmail.com>
2022-07-26 16:20:52 +0100

    added project 02 iteration

--- js/main.js
+++ js/main.js
@@ -17,36 +17,60 @@ document.getElementById('add-comment').addEventListener('click', (event) => {
 });
 
 
-// COMMENTS ITERATION 01
-function getNumberOrString(value) {
-    // Convert a string value to a number if possible
-    let number_value = Number(value);
-    if (Number.isNaN(number_value)) {
-        return value
-    } else {
-        return number_value
-    }
+// COMMENTS ITERATION 02
+var inputComment, inputCommenterName;
+
+function getInputs() {
+    if (--window.LoopTrap <= 0) throw "Infinite loop.";
+    inputComment = getNumberOrString(document.getElementById('input-comment2').value);
+    inputCommenterName = getNumberOrString(document.getElementById('input-commenter-name2').value);
 }
 
-document.getElementById('add-comment2').addEventListener('click', (event) => {
+function renderComment() {
+    if (--window.LoopTrap <= 0) throw "Infinite loop.";
     let element_comments_list = document.getElementById('comments-list2');
     let new_li = document.createElement('li');
     let new_span = document.createElement('span');
-    new_span.innerText = getNumberOrString(document.getElementById('input-commenter-name2').value);
+    new_span.innerText = inputCommenterName;
 
     new_li.appendChild(new_span);
     let new_span2 = document.createElement('span');
-    new_span2.innerText = '  : ';
+    new_span2.innerText = ' : ';
 
     new_li.appendChild(new_span2);
     let new_span3 = document.createElement('span');
-    new_span3.innerText = getNumberOrString(document.getElementById('input-comment2').value);
+    new_span3.innerText = inputComment;
 
     new_li.appendChild(new_span3);
 
     element_comments_list.appendChild(new_li);
+}
+
+function displayError() {
+    if (--window.LoopTrap <= 0) throw "Infinite loop.";
+    let element_error = document.getElementById('error2');
+    element_error.innerText = 'Error: Please enter a Name AND a Comment';
+}
+
+function clearError() {
+    if (--window.LoopTrap <= 0) throw "Infinite loop.";
+    let element_error2 = document.getElementById('error2');
+    element_error2.replaceChildren();
+}
+
+document.getElementById('add-comment2').addEventListener('click', (event) => {
+    clearError();
+    getInputs();
+    if (inputComment == '') {
+        displayError();
+    } else if (inputCommenterName == '') {
+        displayError();
+    } else {
+        renderComment();
+    }
 
 });
+
 
 
 // SHOPPING CART LIGHT MVP
@@ -102,6 +126,115 @@ document.getElementById('add-apple').addEventListener('click', (event) => {
 
 });
 
+
+// SHOPPING CART ITERATION 01
+var productName, productPrice, runningTotal;
+
+// Describe this function...
+function addProductToShoppingCart2(productName, productPrice) {
+    if (--window.LoopTrap <= 0) throw "Infinite loop.";
+    let element_shopping_cart = document.getElementById('shopping-cart2');
+    let new_li = document.createElement('li');
+    let new_span = document.createElement('span');
+    new_span.innerText = 'Item Added: ';
+
+    new_li.appendChild(new_span);
+    let new_span2 = document.createElement('span');
+    new_span2.innerText = productName;
+
+    new_li.appendChild(new_span2);
+    let new_span3 = document.createElement('span');
+    new_span3.innerText = '.....';
+
+    new_li.appendChild(new_span3);
+    let new_span4 = document.createElement('span');
+    new_span4.innerText = 'Price: £';
+
+    new_li.appendChild(new_span4);
+    let new_span5 = document.createElement('span');
+    new_span5.innerText = productPrice;
+
+    new_li.appendChild(new_span5);
+
+    element_shopping_cart.appendChild(new_li);
+    runningTotal.push(productPrice);
+}
+
+// Describe this function...
+function updateTotalCost() {
+    if (--window.LoopTrap <= 0) throw "Infinite loop.";
+    let element_total_price = document.getElementById('total-price2');
+    element_total_price.replaceChildren();
+    let new_span6 = document.createElement('span');
+    new_span6.innerText = '£';
+
+    element_total_price.appendChild(new_span6);
+    let new_span7 = document.createElement('span');
+    new_span7.innerText = runningTotal.reduce((a, b) => a + b, 0);
+
+    element_total_price.appendChild(new_span7);
+}
+
+
+runningTotal = [];
+updateTotalCost();
+
+
+document.getElementById('empty-shopping-cart2').addEventListener('click', (event) => {
+    let element_shopping_cart2 = document.getElementById('shopping-cart2');
+    element_shopping_cart2.replaceChildren();
+    runningTotal = [];
+    updateTotalCost();
+
+});
+
+document.getElementById('add-banana2').addEventListener('click', (event) => {
+    addProductToShoppingCart2('Banana🍌', 0.2);
+    updateTotalCost();
+
+});
+
+document.getElementById('add-mango2').addEventListener('click', (event) => {
+    addProductToShoppingCart2('Mango🥭', 0.85);
+    updateTotalCost();
+
+});
+
+document.getElementById('add-pineapple2').addEventListener('click', (event) => {
+    addProductToShoppingCart2('Pineapple🍍', 1.25);
+    updateTotalCost();
+
+});
+
+document.getElementById('add-kiwi2').addEventListener('click', (event) => {
+    addProductToShoppingCart2('Kiwi🥝', 0.4);
+    updateTotalCost();
+
+});
+
+document.getElementById('add-watermelon2').addEventListener('click', (event) => {
+    addProductToShoppingCart2('Watermelon🍉', 2.5);
+    updateTotalCost();
+
+});
+
+document.getElementById('add-peach2').addEventListener('click', (event) => {
+    addProductToShoppingCart2('Peach🍑', 0.35);
+    updateTotalCost();
+
+});
+
+document.getElementById('add-strawberries2').addEventListener('click', (event) => {
+    addProductToShoppingCart2('Stawberries🍓', 2);
+    updateTotalCost();
+
+});
+
+document.getElementById('add-apple2').addEventListener('click', (event) => {
+    addProductToShoppingCart2('Apple🍏', 0.25);
+    updateTotalCost();
+
+});
 
 // IMAGE CAROUSEL MVP
 var imagesArray, temporary;
